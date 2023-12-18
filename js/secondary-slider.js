8d19e85e09e3e9caa628cf0b1b98ab3c50921eb7
Immanity163 <alexeikurbatov2005@gmail.com>
2023-12-18 20:10:20 +0300

    second slider added

---
+++ js/secondary-slider.js
@@ -0,0 +1,10 @@
+$(".multiple-items").slick({
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:"<img class='a-left arrow prev slick-prev' src='icons/arrow-l-button.svg'>",
+    nextArrow:"<img class='a-right arrow next slick-next' src='icons/arrow-r-button.svg'>",
+  });
+  
